474eb84f870f9198048a377ded1fb847a9b40731
jjliu15 <jamesjliu@google.com>
2022-10-31 21:57:35 -0700

    Add dart API and mapping from dart -> platform

--- packages/google_mobile_ads/lib/src/nativetemplates/native_template_style.dart
+++ packages/google_mobile_ads/lib/src/nativetemplates/native_template_style.dart
@@ -1,10 +1,36 @@
-enum FontStyle {
+import 'dart:ui';
 
-}
+import 'package:google_mobile_ads/src/nativetemplates/template_type.dart';
+import 'package:google_mobile_ads/src/nativetemplates/native_template_text_style.dart';
 
-enum TemplateStyle {
+/// Style options for native templates.
+///
+/// Can be used when loading a [NativeAd].
+class NativeTemplateStyle {
 
-}
-class TextStyle {
+  /// The [TemplateType] to use.
+  TemplateType? templateType;
 
+  /// The [NativeTemplateTextStyle] for the call to action.
+  NativeTemplateTextStyle? callToActionTextStyle;
+
+  /// The [NativeTemplateTextStyle] for the primary text.
+  NativeTemplateTextStyle? primaryTextStyle;
+
+  /// The [NativeTemplateTextStyle] for the second row of text in the template.
+  ///
+  /// All templates have a secondary text area which is populated either by the
+  /// body of the ad or by the rating of the app.
+  NativeTemplateTextStyle? secondaryTextStyle;
+
+  /// The [NativeTemplateTextStyle] for the third row of text in the template.
+  ///
+  /// The third row is used to display store name or the default tertiary text.
+  NativeTemplateTextStyle? tertiaryTextStyle;
+
+  /// The background color.
+  Color? mainBackgroundColor;
+
+  /// The corner radius for the icon view and call to action (iOS only).
+  double? cornerRadius;
 }
