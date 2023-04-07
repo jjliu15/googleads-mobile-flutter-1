e4d93b1cc6a75cf1cd977550f73e85a690f8524f
jjliu15 <jamesjliu@google.com>
2023-04-07 12:51:34 -0700

    temp commit

--- packages/google_mobile_ads/example/lib/auto_sizing_ad_widget_example.dart
+++ packages/google_mobile_ads/example/lib/auto_sizing_ad_widget_example.dart
@@ -39,45 +39,61 @@ class _AutoSizingAdWidgetExampleState extends State<AutoSizingAdWidgetExample> {
         title: Text('Auto sizing ad widget example'),
       ),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: ListView.separated(
-            itemCount: 10,
-            separatorBuilder: (BuildContext context, int index) {
-              return Container(
-                height: 40,
-              );
-            },
-            itemBuilder: (BuildContext context, int index) {
-              if (index == 3 && _nativeAd != null && _nativeIsLoaded) {
-                return Align(
-                  alignment: Alignment.center,
-                  child: AutoSizingAdWidget(ad: _nativeAd!),
-                );
-              }
-
-              if (index == 5 && _bannerAd != null && _bannerIsLoaded) {
-                return Container(width: 500, height: 200, child: AutoSizingAdWidget(ad: _bannerAd!));
-                // return Align(
-                //     alignment: Alignment.center,
-                //     child: AutoSizingAdWidget(ad: _bannerAd!),
-                // );
-              }
-              return Text(
-                Constants.placeholderText,
-                style: TextStyle(fontSize: 24),
-              );
-            },
-          ),
+        child: Column(
+          children: [Container(), Container()
+            // _bannerIsLoaded
+            //     ? Container(width: 500, height: 200, child: AdWidget(ad: _bannerAd!))
+            //     : Container(),
+          ],
         ),
-      ));
+        // child: Padding(
+        //   padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        //   child: ListView.separated(
+        //     itemCount: 10,
+        //     separatorBuilder: (BuildContext context, int index) {
+        //       return Container(
+        //         height: 40,
+        //       );
+        //     },
+        //     itemBuilder: (BuildContext context, int index) {
+        //       if (index == 3 && _nativeAd != null && _nativeIsLoaded) {
+        //         return Align(
+        //           alignment: Alignment.center,
+        //           child: AutoSizingAdWidget(ad: _nativeAd!),
+        //         );
+        //       }
+        //
+        //       if (index == 5 && _bannerAd != null && _bannerIsLoaded) {
+        //         return Container(width: 500, height: 200, child: AdWidget(ad: _bannerAd!));
+        //         // return Align(
+        //         //     alignment: Alignment.center,
+        //         //     child: AutoSizingAdWidget(ad: _bannerAd!),
+        //         // );
+        //       }
+        //       return Text(
+        //         Constants.placeholderText,
+        //         style: TextStyle(fontSize: 24),
+        //       );
+        //     },
+        //   ),
+        // ),
+      ),
+    bottomNavigationBar: SafeArea(
+      left: true,
+      right: true,
+      bottom: true,
+      child: _bannerIsLoaded
+          ? Container(alignment: Alignment.center, width: 468, height: 60, child: SizedBox(child: AdWidget(ad: _bannerAd!)))
+          : Container(),
+    ),
+  );
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
     // Create the ad objects and load ads.
     _bannerAd = BannerAd(
-        size: AdSize.banner,
+        size: AdSize.fullBanner,
         adUnitId: Platform.isAndroid
             ? 'ca-app-pub-3940256099942544/6300978111'
             : 'ca-app-pub-3940256099942544/2934735716',
